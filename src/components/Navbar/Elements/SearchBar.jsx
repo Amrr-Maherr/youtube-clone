b832cd407802f4ea607f2f7d439f1f57f485159d
Amrr-Maherr <amrr.maherr242gmail.com>
2025-09-24 23:09:46 +0300

    style: update search icon cursor behavior
    - Changed cursor style based on search query state.
    - When input is empty, search icon shows `cursor-not-allowed`.
    - When input has text, search icon changes to `cursor-pointer`.
    - Improved UX by giving visual feedback on search availability.

--- src/components/Navbar/Elements/SearchBar.jsx
+++ src/components/Navbar/Elements/SearchBar.jsx
@@ -22,7 +22,7 @@ const SearchBar = () => {
   
   return (
     <div className="flex-grow max-w-[500px] mx-4 relative">
-      <form  className="relative">
+      <form className="relative">
         {searchQuery && (
           <X
             className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#aaaaaa] cursor-pointer hover:text-white"
@@ -38,7 +38,9 @@ const SearchBar = () => {
           className="w-full pl-10 pr-10 py-2 bg-[#121212] rounded-full border border-[#303030] text-white focus:ring-2 focus:ring-[#1a73e8] font-roboto text-[16px] placeholder-[#aaaaaa] text-right"
         />
         <Search
-          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[#aaaaaa] cursor-pointer hover:text-white"
+          className={`absolute left-3 top-1/2 transform -translate-y-1/2 text-[#aaaaaa] ${
+            searchQuery.length === 0 ? "cursor-not-allowed" : "cursor-pointer"
+          } hover:text-white`}
           size={20}
         />
       </form>
