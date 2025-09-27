b5578954bba8626784ff3c4dc4655f257adab1da
Amrr-Maherr <amrr.maherr242gmail.com>
2025-09-27 20:18:14 +0300

    update

--- src/app/Channel/Elements/ChannelVideosSection/ChannelVideosSection.jsx
+++ src/app/Channel/Elements/ChannelVideosSection/ChannelVideosSection.jsx
@@ -2,16 +2,18 @@ import ChannelVideoCard from "../ChannelVideoCard";
 
 function ChannelVideosSection({ videos }) {
   return (
-    <section className="mt-8">
-      <h2 className="text-white text-[20px] md:text-[24px] font-bold mb-4">
-        Channel Videos
-      </h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {videos?.map((video) => (
-          <ChannelVideoCard video={video} key={video.id} />
-        ))}
-      </div>
-    </section>
+    videos && (
+      <section className="mt-8">
+        <h2 className="text-white text-[20px] md:text-[24px] font-bold mb-4">
+          Channel Videos
+        </h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {videos?.map((video) => (
+            <ChannelVideoCard video={video} key={video.id} />
+          ))}
+        </div>
+      </section>
+    )
   );
 }
 
