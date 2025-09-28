1feb6103ed4d783a5912427e7a311359a7cb574c
Amrr-Maherr <amrr.maherr242gmail.com>
2025-09-28 23:26:33 +0300

    update

--- src/components/Navbar/Elements/SearchResult.jsx
+++ src/components/Navbar/Elements/SearchResult.jsx
@@ -5,7 +5,7 @@ function SearchResult({ data, loading, searchQuery }) {
   if (loading) {
     return (
       <>
-        <div className="absolute top-10 z-50 flex items-center justify-center !h-fit w-full bg-[#303030]">
+        <div className="absolute top-10 z-50 flex items-center justify-center h-[100px] md:h-[500px] w-full bg-[#303030]">
           <MoonLoader color="red" />
         </div>
       </>
@@ -15,15 +15,15 @@ function SearchResult({ data, loading, searchQuery }) {
     <div
       className={`${
         searchQuery ? "absolute" : "hidden"
-      }  top-10 z-50 !h-fit w-full bg-[#303030] rounded-md`}
+      } top-10 z-50 w-full bg-[#303030] rounded-md h-[300px] md:h-[500px] overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-800`}
     >
-      {data.slice(0, 8).map((ele) => (
+      {data.map((ele) => (
         <Link
           href={`/VideoDetails/${ele.id.videoId}`}
           key={ele.id.videoId || ele.id}
           onClick={() => dispatch(clearSearchResults())}
         >
-          <div className="flex items-center gap-3 p-4 hover:bg-[#2a2a2a] cursor-pointer transition-colors duration-150">
+          <div className="flex items-center gap-3 p-4 hover:bg-[#2a2a2a] cursor-pointer transition-colors duration-150 over">
             <img
               src={ele?.snippet?.thumbnails?.default?.url}
               alt={ele?.snippet?.title}
